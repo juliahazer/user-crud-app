f895b767620c957fb10e512eb01f34b01e19334a
Julia Hazer <juliahazer@yahoo.com>
2017-05-19 17:03:26 -0700

    added CSRF token and flash message

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_modus import Modus
 import os
 
 app = Flask(__name__)
+#CSRF protection
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'postgres://localhost/userdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -55,6 +58,7 @@ def index():
     db.session.commit()
     return redirect(url_for('index'))
 
+  flash("Welcome!") #add flash message  
   users = User.query.all()
   return render_template('index.html', users = users)
 
